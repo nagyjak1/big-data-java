8583011b82f3aedf45fafdea085b188c130264b1
haitoo <jiahao20021128@gmail.com>
2023-10-30 12:15:09 +0000

    Added BookDownloader class

---
+++ Crawler/src/main/java/BookDownloader.java
@@ -0,0 +1,28 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class BookDownloader {
+    public static final String bookUrl = "https://www.gutenberg.org/cache/epub/";
+    public static final String page = "/pg";
+    public static final String extension = ".txt";
+    public static final String directory = "Crawler/src/main/resources/datalake";
+
+    private void bookDownloader(int booksNumber, String directory) throws IOException {
+        for (int i = 1; i < booksNumber; i++) {
+            URL url = new URL(bookUrl + i + page + i + extension);
+            File file = new File(directory + "/book" + i + extension);
+            FileUtils.copyURLToFile(url, file);
+        }
+    }
+
+    private void bookDownloader2(int booksNumber) throws IOException {
+        bookDownloader(booksNumber, directory);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new BookDownloader().bookDownloader(10, directory);
+    }
+}
